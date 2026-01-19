4b3ae65e5c2f7d3c6f7cb3e67792270aaf525846
nazilatara38-cloud <nazilatara38@gmail.com>
2026-01-19 20:39:32 +0800

    Add files via upload

---
+++ #latihanbreakstetmen.py
@@ -0,0 +1,10 @@
+#latihan break statement
+#break steatment digunakan untuk menghentikan perulangan secara tiba tiba saat kondisis tertentu
+
+n = 5 
+while n < 20:
+  print(n)
+  if n == 9:
+    break
+  n += 2
+print()
